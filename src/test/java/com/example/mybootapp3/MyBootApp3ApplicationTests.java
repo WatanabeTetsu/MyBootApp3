7bc92ef473338754d4aefd5dc90aaf4d7c2826d6
WatanabeTetsu <nnima9981@gmail.com>
2025-05-20 11:17:16 +0900

    kadai4 commit

---
+++ src/test/java/com/example/mybootapp3/MyBootApp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mybootapp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBootApp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
