1fc8a879ca4c17dd9008ad47e760114c60d18716
WatanabeTetsu <nnima9981@gmail.com>
2025-05-21 12:17:42 +0900

    commit

--- src/main/java/jp/te4a/spring/boot/myapp5/mybootapp5/MyBootApp5Application.java
+++ src/main/java/jp/te4a/spring/boot/myapp5/mybootapp5/MyBootApp5Application.java
@@ -1,13 +1,13 @@
-package com.example.mybootapp3;
+package jp.te4a.spring.boot.myapp5.mybootapp5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyBootApp3Application {
+public class MyBootApp5Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyBootApp3Application.class, args);
+		SpringApplication.run(MyBootApp5Application.class, args);
 	}
 
 }
--- src/test/java/jp/te4a/spring/boot/myapp5/mybootapp5/MyBootApp5ApplicationTests.java
+++ src/test/java/jp/te4a/spring/boot/myapp5/mybootapp5/MyBootApp5ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.mybootapp3;
+package jp.te4a.spring.boot.myapp5.mybootapp5;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MyBootApp3ApplicationTests {
+class MyBootApp5ApplicationTests {
 
 	@Test
 	void contextLoads() {
